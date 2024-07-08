4c46d4d54a108fea21dc55867229970901097f4e
Rabbit <dmitrochencko>
2024-07-08 23:31:39 +0300

    hw08-serialization

---
+++ hw08-io/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -0,0 +1,34 @@
+package ru.otus.dataprocessor;
+
+import com.google.gson.Gson;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Map;
+
+public class FileSerializer implements Serializer {
+
+    private final String fileName;
+    private static final  Gson gson = new Gson();
+
+    public FileSerializer(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void serialize(Map<String, Double> data) {
+        // формирует результирующий json
+        String jsonString = gson.toJson(data);
+        writeStringToFile(jsonString);
+    }
+
+    private void writeStringToFile(String str)
+    {
+        try(OutputStream outputStream = new FileOutputStream(fileName)) {
+            outputStream.write(str.getBytes());
+        } catch (IOException e) {
+            throw new FileProcessException(e);
+        }
+    }
+}
