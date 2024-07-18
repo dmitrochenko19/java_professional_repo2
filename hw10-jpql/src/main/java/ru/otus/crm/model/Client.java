301622fb94e79c86bf1b4aa68878a06e5fb53767
Rabbit <dmitrochencko>
2024-07-19 00:36:22 +0300

    completed task

--- hw10-jpql/src/main/java/ru/otus/crm/model/Client.java
+++ hw10-jpql/src/main/java/ru/otus/crm/model/Client.java
@@ -1,12 +1,7 @@
 package ru.otus.crm.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,6 +23,12 @@ public class Client implements Cloneable {
     @Column(name = "name")
     private String name;
 
+    @OneToOne(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private Address address;
+
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<Phone> phones;
+
     public Client(String name) {
         this.id = null;
         this.name = name;
@@ -38,14 +39,24 @@ public class Client implements Cloneable {
         this.name = name;
     }
 
-    public <E> Client(Long id, String name, Address address, List<Phone> phones) {
-        throw new UnsupportedOperationException();
+    public Client(Long id, String name, Address address, List<Phone> phones) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phones = phones;
+        phones.forEach(phone -> phone.setClient(this));
+        address.setClient(this);
     }
 
     @Override
     @SuppressWarnings({"java:S2975", "java:S1182"})
     public Client clone() {
-        return new Client(this.id, this.name);
+        Client client = new Client(this.id, this.name);
+        client.phones = this.phones;
+        phones.forEach(phone -> phone.setClient(client));
+        client.address = this.address;
+        address.setClient(client);
+        return client;
     }
 
     @Override
