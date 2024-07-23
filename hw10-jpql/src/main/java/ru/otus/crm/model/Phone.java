5018f4855333fc95ff0270766116b03c57a4b385
dmitrochenko19 <131820269+dmitrochenko19@users.noreply.github.com>
2024-07-23 14:16:41 +0300

    Hw10 jpql (#6)
    hw10-jpql
    ---------
    
    Co-authored-by: Rabbit <dmitrochencko>

---
+++ hw10-jpql/src/main/java/ru/otus/crm/model/Phone.java
@@ -0,0 +1,35 @@
+package ru.otus.crm.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "phone")
+public class Phone implements Cloneable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "number")
+    private String number;
+
+    @ManyToOne()
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    public Phone(Long id, String number) {
+        this.id = id;
+        this.number = number;
+    }
+
+    @Override
+    protected Phone clone() throws CloneNotSupportedException {
+        Phone phone =  new Phone(this.id, this.number);
+        return phone;
+    }
+}
